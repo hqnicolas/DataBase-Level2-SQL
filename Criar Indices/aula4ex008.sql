b6ee69cffe5e1453c2a0f1ede695447e88a617a6
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-08-30 13:19:33 -0300

    Aula 4

--- Criar Indices/aula4ex008.sql
+++ Criar Indices/aula4ex008.sql
@@ -1,3 +1,21 @@
 -- Com base nas duas consultas geradas (SELECT sem WHERE e SELECT com WHERE Treviso) e compare a
 -- quantidade de registros retornadas em cada uma, os resultados dos SET STATISTICS e dos planos de acesso.
 -- Qual a diferença encontrada entre eles?
+
+SELECT COUNT(1)
+FROM MICRODADOS_ENEM_2021_SC WHERE NO_MUNICIPIO_ESC = 'Treviso';
+go
+
+SELECT COUNT(1)
+FROM MICRODADOS_ENEM_2021_SC;
+go
+
+--A consulta que retorna 10 linhas e a consulta que retorna 26394 linhas
+--precisam ler a mesma quantidade de pÃ¡gina de dados.
+--ou seja, com WHERE ou SEM WHERE o "peso" de ambas as consultas
+--Ã© o mesmo.
+
+--Table 'MICRODADOS_ENEM_2021_SC'. Scan count 1, logical reads 2981, physical reads 0, page server reads 0, read-ahead reads 0, page server read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob page server reads 0, lob read-ahead reads 0, lob page server read-ahead reads 0.
+--Table 'MICRODADOS_ENEM_2021_SC'. Scan count 1, logical reads 2981, physical reads 0, page server reads 0, read-ahead reads 0, page server read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob page server reads 0, lob read-ahead reads 0, lob page server read-ahead reads 0.
+
+--Ambos fazem Clustered Index Scan
