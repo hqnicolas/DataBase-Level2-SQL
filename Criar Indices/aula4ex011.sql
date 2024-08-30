d2957f806a660dc470d93b0ca92079d3dd4ff824
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-08-30 13:20:48 -0300

    Aula 4

--- Criar Indices/aula4ex011.sql
+++ Criar Indices/aula4ex011.sql
@@ -6,3 +6,13 @@ Uma forma de resolver esse problema é criar um índice não clusterizado na col
  como já foi feito. Isso permitirá que o SQL Server use o índice para encontrar rapidamente os
   locais das linhas com base no valor de NO_MUNICIPIO_ESC e, em seguida, 
   recuperar as informações dessas linhas diretamente da tabela.
+
+drop index MICRODADOS_ENEM_2021_SC.idx_NO_MUNICIPIO_ESC;
+go
+create index idx_NO_MUNICIPIO_ESC on MICRODADOS_ENEM_2021_SC (NO_MUNICIPIO_ESC) INCLUDE (TP_ESCOLA);
+go
+
+--Gerar os planos de acesso e os dados de set statistics para ambas as consultas.
+SELECT NO_MUNICIPIO_ESC, TP_ESCOLA
+FROM MICRODADOS_ENEM_2021_SC WHERE NO_MUNICIPIO_ESC = 'Treviso';
+go
