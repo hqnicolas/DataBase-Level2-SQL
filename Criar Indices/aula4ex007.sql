5d8137744cdf846bd2143b45908a6d6405818ab6
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-08-30 13:19:03 -0300

    Aula 4

--- Criar Indices/aula4ex007.sql
+++ Criar Indices/aula4ex007.sql
@@ -6,3 +6,9 @@ SET STATISTICS TIME ON;
 SELECT  * FROM MICRODADOS_ENEM_2021_SC WHERE NO_MUNICIPIO_ESC = 'Treviso';
 SET STATISTICS IO OFF;
 SET STATISTICS TIME OFF;
+
+
+--Table 'MICRODADOS_ENEM_2021_SC'. Scan count 1, logical reads 2981, physical reads 0, page server reads 0, read-ahead reads 42, page server read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob page server reads 0, lob read-ahead reads 0, lob page server read-ahead reads 0.
+
+-- SQL Server Execution Times:
+--   CPU time = 0 ms,  elapsed time = 38 ms.
