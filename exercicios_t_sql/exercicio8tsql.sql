6346193cc8d43587d74062415f8dc61b4ab1153a
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-10-08 23:15:32 -0300

    T-SQL

--- exercicios_t_sql/exercicio8tsql.sql
+++ exercicios_t_sql/exercicio8tsql.sql
@@ -2,6 +2,42 @@
 -- necessárias para se cadastrar um sinistro, como parâmetros de entrada. Validar antes de cadastrar o
 -- sinistro, se o carro existe. Caso exista, prosseguir com o INSERT. Caso não exista, informar via PRINT que o
 -- carro não existe. 
--- A lista da stored procedures deverá ter as seguintes colunas:
--- Nome do curso, ranking, qt_aluno
--- A coluna qt_sinistro deverá possuir a quantidade de alunos relativos ao curso
+
+
+
+CREATE OR ALTER PROCEDURE  RegistrarSinistro 
+    @cod_sinistro INT,
+    @placa VARCHAR(10),
+    @data_sinistro DATE,
+    @hora_sinistro TIME(7),
+    @local_sinistro VARCHAR(100),
+    @condutor VARCHAR(100)
+AS
+BEGIN
+  -- Validar se o carro existe antes de registrar o sinistro
+  DECLARE @existeCarro BIT = 0;
+  SELECT @existeCarro = 1 
+  FROM carro
+  WHERE placa = @placa;
+
+  IF @existeCarro = 0
+  BEGIN
+    PRINT 'Erro: O carro com a placa informado não existe.'
+    RETURN;
+  END
+
+  -- Inserir o sinistro no banco de dados
+  INSERT INTO sinistro (cod_sinistro, placa, data_sinistro, hora_sinistro, local_sinistro, condutor)
+  VALUES (@cod_sinistro, @placa, @data_sinistro, @hora_sinistro, @local_sinistro, @condutor);
+
+  PRINT 'Sinistro registrado com sucesso!';
+END;   
+GO
+
+----------------------------- chame a procedure para cadastrar o sinistro
+
+EXEC RegistrarSinistro @cod_sinistro = 123, @placa = 'ABC1234', 
+                               @data_sinistro = '2023-10-26', 
+                               @hora_sinistro = '15:30:00', 
+                               @local_sinistro = 'Rua dos Carmosos, 123', 
+                               @condutor = 'João Silva'; 
