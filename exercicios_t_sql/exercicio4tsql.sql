f13344911bc4a9d472980a9a15756047ea4c4d1c
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-10-08 22:53:42 -0300

    T-SQL

--- exercicios_t_sql/exercicio4tsql.sql
+++ exercicios_t_sql/exercicio4tsql.sql
@@ -5,3 +5,34 @@
 -- existem sinistros atrelados a placa informada.
 
 
+CREATE OR ALTER PROCEDURE sp_consultar_sinistros_por_placa 
+    @placa VARCHAR(10)
+AS
+BEGIN
+    DECLARE @totalSinistros INT;
+
+    SELECT @totalSinistros = COUNT(*)
+    FROM carro c
+    JOIN sinistro s ON c.placa = s.placa
+    WHERE c.placa = @placa;
+
+    IF @totalSinistros = 0
+        PRINT 'Não existem sinistros atrelados à placa informada.';
+    ELSE
+        BEGIN
+            SELECT c.placa, s.data_sinistro, s.hora_sinistro, s.condutor
+            FROM carro c
+            JOIN sinistro s ON c.placa = s.placa
+            WHERE c.placa = @placa;
+        END; 
+END;
+
+
+
+------------ execute o procedimento
+SELECT c.placa
+    FROM carro c
+
+EXEC sp_consultar_sinistros_por_placa @placa = 'EEE1056';  -- sem sinistro
+
+EXEC sp_consultar_sinistros_por_placa @placa = 'ALD3834'; -- com sinistro
