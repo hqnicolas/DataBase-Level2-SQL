e94c2eb051375a9c739fb23f32db10488c938d55
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-10-15 19:46:15 -0300

    T-SQL

--- exercicios_t_sql/exercicio11tsql.sql
+++ exercicios_t_sql/exercicio11tsql.sql
@@ -5,3 +5,28 @@
 -- Continuando a execução do código fora da stored procedure, crie um SELECT na tabela carro, mostrando
 -- todos os dados do carro e passado como parâmetro WHERE a placa do carro que foi retornada pela
 -- procedure (variável de saída).
+
+CREATE OR ALTER PROC pr_consulta_placa_sinistro (@cod_sinistro int, @placa varchar(7) OUTPUT) AS
+BEGIN
+SELECT @placa = placa
+FROM sinistro
+WHERE cod_sinistro = @cod_sinistro
+IF @placa is null
+PRINT 'NAO EXISTE SINISTRO CADASTRADO PARA A PLACA INFORMADA'
+
+--nao precisa da clausula ELSE porque o @nome ja foi populado
+--pela consulta cima.
+--Ex.:
+--SE @nome for nulo
+-- imprima mensagem de ausencia da placa
+--SENAO
+-- atualize @placa com a placa do carro (feito antes do IF)
+END
+GO
+--execucao da stored procedure
+
+DECLARE @placa_carro varchar(50)
+EXEC pr_consulta_placa_sinistro 202255503, @placa_carro OUTPUT
+IF @placa_carro is not null
+SELECT * FROM carro WHERE placa = @placa_carro
+GO
